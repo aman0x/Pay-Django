e305521f81be808e25fd456ff7e944cc2ac451c0
aman0x <amanchandel4@gmail.com>
2024-06-27 15:51:57 +0530

    statistic apis added

--- statisticApp/api/urls.py
+++ statisticApp/api/urls.py
@@ -7,6 +7,9 @@ router = routers.DefaultRouter()
 urlpatterns = [
     path('', include(router.urls)),
     path('transaction-dashboard/', views.StatisticTransactionDashboardViewSet.as_view(), name="statistic-transaction-dashboard"),
-    path('invoice-sent-dashboard/', views.StatisticTransactionDashboardViewSet.as_view(), name="invoice-sent-dashboard"),
-    path('invoice-received-dashboard/', views.StatisticTransactionDashboardViewSet.as_view(), name="invoice-received-dashboard"),
+    path('invoice-sent-dashboard/', views.StatisticInvoiceSentDashboardViewSet.as_view(), name="statistic-invoice-sent-dashboard"),
+    path('invoice-received-dashboard/', views.StatisticInvoiceReceivedDashboardViewSet.as_view(), name="statistic- invoice-received-dashboard"),
+    path('transaction-list/', views.StatisticTransactionListViewSet.as_view(), name="statistic-transaction-list"),
+    path('invoice-sent-list/', views.StatisticTransactionListViewSet.as_view(), name="statistic-invoice-sent-list"),
+    path('invoice-received-list/', views.StatisticTransactionListViewSet.as_view(), name="statistic-invoice-received-list"),
 ]
--- statisticApp/api/views.py
+++ statisticApp/api/views.py
@@ -27,3 +27,103 @@ class StatisticTransactionDashboardViewSet(views.APIView):
         }
         serializer = StatisticTransactionDashboardSerializer(data)
         return Response(serializer.data)
+    
+class StatisticInvoiceSentDashboardViewSet(views.APIView):
+    permission_classes = [permissions.AllowAny]
+
+    def get(self, request):
+        data = {
+            "card_number" : "1234567824681257",
+            "incomes" : 24000.70,
+            "total_sended_invoices" : 1200,
+            "today_sended_invoices" : 4,
+            "succeeded" : 12,
+            "in_progress" : 12,
+            "failed" : 12,
+            "refunded" : 12,
+            "total_succeeded" : 24000.24,
+            "today_succeeded" : 1290,
+            "total_in_progress" : 240.70,
+            "today_in_progress" : 12,
+            "total_failed" : 240.70,
+            "today_failed" : 12,
+            "total_successful_invoices" : 500,
+            "total_recieved_amount" : 24000.24,
+            "total_paid_tax" : 240.24
+        }
+        serializer = StatisticInvoiceSentDashboardSerializer(data)
+        return Response(serializer.data)
+    
+class StatisticInvoiceReceivedDashboardViewSet(views.APIView):
+    permission_classes = [permissions.AllowAny]
+
+    def get(self, request):
+        data = {
+            "card_number" : "1234567824681257",
+            "incomes" : 24000.70,
+            "total_received_invoices" : 1200,
+            "today_received_invoices" : 4,
+            "succeeded" : 12,
+            "in_progress" : 12,
+            "failed" : 12,
+            "refunded" : 12,
+            "total_paid" : 24000.24,
+            "today_paid" : 1290,
+            "total_in_progress" : 240.70,
+            "today_in_progress" : 12,
+            "total_failed" : 240.70,
+            "today_failed" : 12,
+            "total_successful_invoices" : 500,
+            "total_paid_amount" : 24000.24,
+            "total_paid_tax" : 240.24
+        }
+        serializer = StatisticInvoiceReceivedDashboardSerializer(data)
+        return Response(serializer.data)
+    
+class StatisticTransactionListViewSet(views.APIView):
+    permission_classes = [permissions.AllowAny]
+
+    def get(self, request):
+        data = {
+            "date" : "2024-06-21T10:21:17.116232Z",
+            "customer": "John P.",
+            "type" : "Vendor_Payment",
+            "bank": "HDFC Bank , KODAD, HDFC0001642",
+            "status" : "Succeeded",
+            "sum": 24000.24,
+            "commission" : 240.24
+        }
+        serializer = StatisticTransactionListSerializer(data)
+        return Response(serializer.data)
+    
+class StatisticInvoiceSentListViewSet(views.APIView):
+    permission_classes = [permissions.AllowAny]
+
+    def get(self, request):
+        data = {
+            "date" : "2024-06-21T10:21:17.116232Z",
+            "sent_to": "John P.",
+            "type" : "Vendor_Payment",
+            "bank": "HDFC Bank , KODAD, HDFC0001642",
+            "status" : "Succeeded",
+            "sum": 24000.24,
+            "commission" : 240.24
+        }
+        serializer = StatisticInvoiceSentListSerializer(data)
+        return Response(serializer.data)
+    
+class StatisticInvoiceReceivedListViewSet(views.APIView):
+    permission_classes = [permissions.AllowAny]
+
+    def get(self, request):
+        data = {
+            "date" : "2024-06-21T10:21:17.116232Z",
+            "sent_from": "John P.",
+            "type" : "Vendor_Payment",
+            "bank": "HDFC Bank , KODAD, HDFC0001642",
+            "status" : "Succeeded",
+            "sum": 24000.24,
+            "commission" : 240.24
+        }
+        serializer = StatisticInvoiceReceivedListSerializer(data)
+        return Response(serializer.data)
